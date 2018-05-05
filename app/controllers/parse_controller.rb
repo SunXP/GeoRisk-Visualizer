5d9e53d98704038653c1261b02f4a10a95ca41ef
Wai <12164553+Wai-Lau@users.noreply.github.com>
2018-05-05 13:35:42 -0400

    post requests work on /parse

--- app/controllers/parse_controller.rb
+++ app/controllers/parse_controller.rb
@@ -1,9 +1,18 @@
 class ParseController < ApplicationController
   def index
-    puts params[:id]
-    @id = params[:id]
+  end
+
+  def parse
+    puts params[:name]
     respond_to do |format|
-      msg = { :status => "ok", :message => "Success!", :html => "<b>...</b>" }
+      msg = {
+        :status => "ok",
+        :message => "Success!",
+        :html => "<b>...</b>",
+        :mapData => {
+          :geoJson => params[:name]
+        }
+      }
       format.json  { render :json => msg }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'parse/:id/', to: 'parse#index'
-  match 'parse/:id/' => 'parse#index', via: :post
+  match 'parse/' => 'parse#parse', via: :post
   #get 'welcome/:id/', to: 'welcome#index'
   root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
